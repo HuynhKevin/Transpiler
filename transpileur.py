33005d44d6d56191b4197c7603821231273df6d5
Kevin Huynh <kevin.huynh@epita.fr>
2021-12-14 21:12:48 +0100

    wip

--- transpileur.py
+++ transpileur.py
@@ -1,6 +1,19 @@
 import textwrap
 import os
 import joblib
+import pandas as pd  
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+def save_model():
+    data = pd.read_csv('./tumors.csv')
+    X = data.iloc[:, :2].values
+    y = data.iloc[:, 2:].values
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+    regr = LinearRegression().fit(X_train, y_train)
+    filename = 'model.joblib'
+    joblib.dump(regr, open(filename, 'wb'))
+    return regr, X_test
 
 def coeff_model(model):
     loaded_model = joblib.load(model)
@@ -48,8 +61,15 @@ def main_functions():
     return c_function
 
 if __name__ == '__main__':
+    model, X_test = save_model()
     f = open("linear_regression.c", "w")
     f.write(predict_function())
     f.write(main_functions())
     f.close()
     os.system("gcc linear_regression.c")
+    print("Sklearn predict: \n", model.predict(X_test[:3]) , "\n")
+    print ("Transpiler predict:")
+    for i in range(len(X_test[:3])):
+        command = "./a.out " + str(X_test[i][0]) + " " +  str(X_test[i][1])
+        os.system(command)
+        print()
